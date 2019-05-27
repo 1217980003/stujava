c80a2877833eacfbf9396acfb44bccb2711bb4e4
1290587630 <1290587630@qq.com>
2019-05-27 17:43:55 +0800

    s

--- app/src/main/java/com/stu/controller/UserController.java
+++ app/src/main/java/com/stu/controller/UserController.java
@@ -3,12 +3,14 @@ package com.stu.controller;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.stu.pojo.User;
 import com.stu.service.IUserService;
+import com.stu.testmybatis.TestMybatis;
 
 @Controller
 @RequestMapping("/user")
@@ -16,7 +18,9 @@ public class UserController {
 
     @Resource
     private IUserService userService;
-
+    
+    private static Logger logger = Logger.getLogger(UserController.class);
+    
     @RequestMapping("/showUser")
     public String toIndex(HttpServletRequest request, Model model) {
         int userId = Integer.parseInt(request.getParameter("id"));
